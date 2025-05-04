71dfd1f43b19f515bab3d1ee90caf229c31c6b65
João Paulo <joaowais@gmail.com>
2025-05-04 20:59:14 -0300

    first commit

---
+++ src/main/java/com/jhops10/devfinder/DevfinderApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.devfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhops10/devfinder/DevfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhops10.devfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
